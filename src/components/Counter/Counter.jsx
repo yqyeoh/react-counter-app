097ac396726debc710d9f7fae48be5444b4dc869
yqyeoh <yqyeoh@hotmail.com>
2019-02-21 16:50:46 +0800

    refactor counter component to a stateless functional component

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Counter extends Component {
-    /*state = { 
-      id: this.props.id,
-      value: this.props.value
-    }*/
-
-    evaluateClass=()=>{
-      return this.props.value > 0? "primary":"warning";
-    }
-
-  render() {
-    return (
-      <React.Fragment>
-        <h2>
-          <span className={`badge badge-${this.evaluateClass()}`}>{this.props.value}</span>
-        </h2>
-        <button onClick={()=>this.props.handleIncrement(this.props.id)} className="btn btn-primary m-2"> + </button>
-        <button onClick={()=>this.props.handleDecrement(this.props.id)} className="btn btn-danger m-2"> - </button>
-        <button onClick={()=>this.props.handleDelete(this.props.id)} className="btn btn-secondary m-2"> Delete </button>
-      </React.Fragment>
-    )
+function Counter(props) {
+  const evaluateClass=()=>{
+    return props.value > 0? "primary":"warning";
   }
+  return (
+    <React.Fragment>
+        <h2>
+          <span className={`badge badge-${evaluateClass()}`}>{props.value}</span>
+        </h2>
+        <button onClick={()=>props.handleIncrement(props.id)} className="btn btn-primary m-2"> + </button>
+        <button onClick={()=>props.handleDecrement(props.id)} className="btn btn-danger m-2"> - </button>
+        <button onClick={()=>props.handleDelete(props.id)} className="btn btn-secondary m-2"> Delete </button>
+      </React.Fragment>
+  )
 }
 
 export default Counter
