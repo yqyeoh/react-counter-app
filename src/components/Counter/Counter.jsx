d8bd41c26021cbba0b9c379c6e996d7779900b8b
yqyeoh <yqyeoh@hotmail.com>
2019-02-21 16:54:05 +0800

    refactor counter to destructure all props

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
 function Counter(props) {
+  const {value, id, handleDecrement, handleIncrement, handleDelete} = props
   const evaluateClass=()=>{
     return props.value > 0? "primary":"warning";
   }
   return (
     <React.Fragment>
         <h2>
-          <span className={`badge badge-${evaluateClass()}`}>{props.value}</span>
+          <span className={`badge badge-${evaluateClass()}`}>{value}</span>
         </h2>
-        <button onClick={()=>props.handleIncrement(props.id)} className="btn btn-primary m-2"> + </button>
-        <button onClick={()=>props.handleDecrement(props.id)} className="btn btn-danger m-2"> - </button>
-        <button onClick={()=>props.handleDelete(props.id)} className="btn btn-secondary m-2"> Delete </button>
+        <button onClick={()=>handleIncrement(id)} className="btn btn-primary m-2"> + </button>
+        <button onClick={()=>handleDecrement(id)} className="btn btn-danger m-2"> - </button>
+        <button onClick={()=>handleDelete(id)} className="btn btn-secondary m-2"> Delete </button>
       </React.Fragment>
   )
 }
