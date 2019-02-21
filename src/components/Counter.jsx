5ab8ea099fac3af0dbb6a4f0d8ef004e99a818f0
yqyeoh <yqyeoh@hotmail.com>
2019-02-21 11:48:28 +0800

    Solution to Part 3: Dynamically change counter style

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,18 +8,23 @@ export class Counter extends Component {
 
     handleIncrement=()=>{
       const newValue = this.state.value + 1
-      this.setState({value:newValue});
+      this.setState({value:newValue})
     }
 
     handleDecrement=()=>{
       const newValue = this.state.value - 1
       this.setState({value:newValue})
     }
+
+    evaluateClass=()=>{
+      return this.state.value > 0? "primary":"warning";
+    }
+
   render() {
     return (
       <React.Fragment>
         <h2>
-          <span className="badge badge-secondary">{this.state.value}</span>
+          <span className={`badge badge-${this.evaluateClass()}`}>{this.state.value}</span>
         </h2>
         <button onClick={this.handleIncrement} className="btn btn-primary m-2"> + </button>
         <button onClick={this.handleDecrement} className="btn btn-danger m-2"> - </button>
